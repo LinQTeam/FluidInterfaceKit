f8d647508c941b3a8566e0472e6a9e041cb721db
entaku <entaku19890818@gmail.com>
2023-12-16 21:44:06 +0900

    fix

--- Sources/FluidStack/Helper/ScrollView+Handling.swift
+++ Sources/FluidStack/Helper/ScrollView+Handling.swift
@@ -84,10 +84,7 @@ final class ScrollController {
     debugPrint("\(timestamp): handleScrollViewEvent")
     // 垂直方向のスクロール（y座標の変更）を無視する
     // TODO: メッセージ画面で参照されているので縦方向のスクロールが発生した時に無限ループになってしまうため、このような対応を入れたが、本来は検知回数を少なくする対応が望ましい。
-    if oldValue.y != scrollView.contentOffset.y {
-      isHandlingEvent = false
-      return
-    }
+
 
     guard isLocking else {
       isHandlingEvent = false
